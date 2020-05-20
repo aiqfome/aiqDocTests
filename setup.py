2780d5ef1d75411196d7a4642264af9bcc545af5
Pedro Matias de Araújo <pedrom.araujo@outlook.com>
2020-05-20 18:42:02 -0300

    add require requests

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     url="https://github.com/aiqfome/aiqDocTests",
     license="LICENSE",
     description="A framework to validate request/response's json and create documentation",
-    install_requires=["cerberus",],
+    install_requires=["cerberus", "requests"],
     package_data={"aiqdoctests": [".aiqdoctests.config", "wait"]},
     include_package_data=True,
     zip_safe=False,
